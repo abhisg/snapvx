456b602a24a588f3d7c63d99f43b66936ac71e0a
Abhijit <abhijit.sharang@gmail.com>
2016-04-20 10:44:02 -0700

    proximal operator support

--- include/ADMM.cpp
+++ include/ADMM.cpp
@@ -3,71 +3,84 @@
 
 ADMM::ADMM()
 {
-	node_objectives.clear();
-	node_constraints.clear();
-	edge_objectives.clear();
-	edge_constraints.clear();
+	node_list.clear();
+	edge_list.clear();
+	node_x_vals.clear();
+	edge_z_vals.clear();
+	edge_u_vals.clear();
+	edge_prox = NONE;
+	node_prox = NONE;
+	prox_node_arg = 0;
+	prox_edge_arg = 0;
+	solver_options = std::map<std::string,double>();
 }
 	
 //TODO : improve the shitty memory management
 void ADMM::LoadNodes(std::vector<LinOp* > &objectives,std::vector<std::vector< LinOp *> > &constraints)
 {
-	for(int i = 0 ; i < objectives.size(); ++i ){
-		node_objectives.push_back(objectives[i]);
-	}
-	for ( int i = 0 ; i < constraints.size(); ++i ){
-		std::vector<LinOp*> cons_vec;
-		std::cout << "Constraint " << i << " " <<  constraints[i].size() << "\n";
-		for ( int j = 0 ; j < constraints[i].size(); j++ ){
-			cons_vec.push_back(constraints[i][j]);
-		}
-		node_constraints.push_back(cons_vec);
-	}
+for ( int i = 0 ; i < objectives.size(); ++i){
+		Node *newnode = new Node;
+		newnode->node_objective = objectives[i];
+		newnode->node_constraints = constraints[i];	//sloppy but we do not reclaim the memory before the end of the python function
+		newnode->z_var_idx = std::vector<std::vector<int> >();
+		newnode->u_var_idx = std::vector<std::vector<int> >();
+		newnode->x_var_idx = std::vector<std::vector<int> >();
+		node_list.push_back(newnode);
+	}	
 }
 
 void ADMM::LoadEdges(std::vector<LinOp* > &objectives,std::vector<std::vector< LinOp *> > &constraints)
 {
-	for(int i = 0 ; i < objectives.size(); ++i ){
-		edge_objectives.push_back(objectives[i]);
-	}
-	for ( int i = 0 ; i < constraints.size(); ++i ){
-		std::vector<LinOp*> cons_vec;
-		std::cout << "Constraint " << i << " " <<  constraints[i].size() << "\n";
-		for ( int j = 0 ; j < constraints[i].size(); j++ ){
-			cons_vec.push_back(constraints[i][j]);
-		}
-		edge_constraints.push_back(cons_vec);
+	for ( int i = 0 ; i < objectives.size(); ++i){
+		Node *newedge = new Edge;
+		newedge->edge_objective = objectives[i];
+		newedge->edge_constraints = constraints[i];	//sloppy but we do not reclaim the memory before the end of the python function
+		newedge->zij_var_idx = std::vector<int>();
+		newedge->zji_var_idx = std::vector<int>();
+		newedge->uij_var_idx = std::vector<int>();
+		newedge->uji_var_idx = std::vector<int>();
+		newedge->xi_var_idx = std::vector<int>();
+		newedge->xj_var_idx = std::vector<int>();
+		edge_list.push_back(newedge);
+	}	
+}
+
+void LoadNodesProximal(ProximalOperator prox,std::vector<std::vector<int> > x_var_idx,std::vector<std::vector<std::vector<int> > > neighbour_var_idx,arg=0)
+{
+	node_prox = prox;
+	prox_node_arg = arg;
+	for ( int i = 0 ; i < x_var_idx.size(); ++i){
+		Node *newnode = new Node;
+		newnode->node_objective = NULL;
+		newnode->node_constraints = std::vector<LinOp *>();
+		newnode->neighbour_var_idx = neighbour_var_idx[i];
+		newnode->x_var_idx = x_var_idx[i];
+		node_list.push_back(newnode);
 	}
 }
 
-
-/*void ADMM::LoadEdges(std::vector<LinOp* > &objectives,std::vector<std::vector< LinOp *> > &constraints)
+void LoadEdgesProximal(ProximalOperator prox,std::vector<std::vector<std::pair<int,int> > > edge_var_idx,std::vector<std::vector<std::pair<int,int> > > node_var_idx,arg=0)
 {
-	for(int i = 0 ; i < objectives.size(); ++i ){
-		//Solution *usol = new Solution;
-		//Solution *zsol = new Solution;
-		edge_objectives.push_back(objectives[i]);
-		//edge_z_vals.push_back(zsol);
-		//edge_u_vals.push_back(usol);
+	edge_prox = prox;
+	prox_edge_arg = arg;
+	for ( int i = 0 ; i < edge_var_idx.size(); ++i ){
+		Edge *newedge = new Edge;
+		newedge->edge_objective = NULL;
+		newedge->edge_constraints = std::vector<LinOp *>();
+		newedge->edge_var_idx = edge_var_idx[i];
+		newedge->node_var_idx = node_var_idx[i];
+		edge_list.push_back(newedge);
 	}
-	for ( int i = 0 ; i < constraints.size(); ++i ){
-		/*std::vector<LinOp *> newcons;
-		for ( int j = 0 ; j < constraints[i].size(); ++j){
-			newcons.push_back(constraints[i][j]);
-		}
-		edge_constraints.push_back(newcons);
-		edge_constraints.push_back(constraints[i]);
-	}
-}*/
+}
 
 void ADMM::Solve()
 {
 	//TODO : full implementation with convergence
 	for(int iter = 0 ; iter < 1; ++iter){
-		for ( int i = 0 ; i < node_objectives.size(); ++i ){
+		for ( int i = 0 ; i < node_list.size(); ++i ){
 			ADMM_x(i);
 		}
-		for ( int i = 0 ; i < edge_objectives.size(); ++i ){
+		for ( int i = 0 ; i < edge_list.size(); ++i ){
 			ADMM_z(i);
 			ADMM_u(i);
 		}
@@ -77,13 +90,20 @@ void ADMM::Solve()
 void ADMM::ADMM_x(int i){
 	//TODO : change the rho of the objective
 	//*node_x_vals[i] = solve(MINIMIZE,node_objectives[i],node_constraints[i],solver_options);
-	Solution soln = solve(MINIMIZE,node_objectives[i],node_constraints[i],solver_options);
+	if ( node_list[i]->node_objective != NULL ){
+		Solution soln = solve(MINIMIZE,node_objectives[i],node_constraints[i],solver_options);
+	}
+	else{
+		//solve using prox operator update
+	}
 }
 
 void ADMM::ADMM_z(int i){
 	//TODO : change the rho of the objective
 	//*edge_z_vals[i] = solve(MINIMIZE,edge_objectives[i],edge_constraints[i],solver_options);
-	Solution soln = solve(MINIMIZE,edge_objectives[i],edge_constraints[i],solver_options);
+	if ( edge_list[i]->edge_objective != NULL ){
+		Solution soln = solve(MINIMIZE,edge_objectives[i],edge_constraints[i],solver_options);
+	}
 }
 
 void ADMM::ADMM_u(int i){
--- include/ADMM.h
+++ include/ADMM.h
@@ -6,16 +6,45 @@
 #include <string>
 #include <utility>
 
+typedef enum ProximalOperator
+{
+	SQUARE,
+	LASSO,
+	NONE
+}ProximalOperator;
+
+typedef struct Node 
+{
+	LinOp* node_objective;
+	std::vector<LinOp *> node_constraints;
+	std::vector<std::vector<int> > neighbour_var_idx;
+	std::vector<int> x_var_idx;
+}Node;
+
+typedef struct Edge
+{
+	LinOp* edge_objective;
+	std::vector<LinOp *> edge_constraints;
+	std::vector<std::pair<int,int> > edge_var_idx;
+	std::vector<std::pair<int,int> > node_var_idx;
+}Edge;
+
 class ADMM
 {
 	protected:
-		std::vector<LinOp* > node_objectives;
+		/*std::vector<LinOp* > node_objectives;
 		std::vector<LinOp* > edge_objectives;
 		std::vector<std::vector< LinOp *> > node_constraints;
-		std::vector<std::vector< LinOp *> > edge_constraints;
-		std::vector<Solution *> node_x_vals;
-		std::vector<Solution *> edge_z_vals;
-		std::vector<Solution *> edge_u_vals;
+		std::vector<std::vector< LinOp *> > edge_constraints;*/
+		std::vector<Node *> node_list;
+		std::vector<Edge *> edge_list;
+		std::vector<Eigen::MatrixXd> node_x_vals;
+		std::vector<Eigen::MatrixXd> edge_z_vals;
+		std::vector<Eigen::MatrixXd> edge_u_vals;
+		ProximalOperator edge_prox;
+		ProximalOperator node_prox;
+		int prox_node_arg;
+		int prox_edge_arg;
 		std::map<std::string,double > solver_options;
 		void ADMM_x(int);
 		void ADMM_z(int);
@@ -23,8 +52,12 @@ class ADMM
 		//void copyLinop(LinOp* const&,LinOp* const&);
 	public:
 		ADMM();
+		/*void LoadNodes(std::vector<LinOp* > &,std::vector<std::vector< LinOp *> > &);
+		void LoadEdges(std::vector<LinOp* > &,std::vector<std::vector< LinOp *> > &);*/
 		void LoadNodes(std::vector<LinOp* > &,std::vector<std::vector< LinOp *> > &);
 		void LoadEdges(std::vector<LinOp* > &,std::vector<std::vector< LinOp *> > &);
+		void LoadNodesProximal(ProximalOperator,std::vector<std::vector<int> >,std::vector<std::vector<std::vector<int> > >,arg);
+		void LoadEdgesProximal(ProximalOperator,std::vector<std::vector<std::pair<int,int> > >,std::vector<std::vector<std::pair<int,int> > >,arg);
 		void Solve();
 		std::vector<std::map<int, Eigen::MatrixXd> > get_node_x_vals();
 		std::vector<std::map<int, Eigen::MatrixXd> > get_edge_z_vals();
